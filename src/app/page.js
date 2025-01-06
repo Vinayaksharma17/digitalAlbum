c103902fccae9161cc1a5099ebd089e85f6a1855
Vinayaksharma17 <vvsharma999@gmail.com>
2025-01-06 20:41:24 +0530

    WIP: fix hydration error

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,11 @@
-import Navbar from "../components/ui/navbar"
-import TypographyH2 from "../components/ui/typography"
+import TypographyH2 from "@/components/ui/typography"
 import { Roboto } from "next/font/google"
 
 const roboto = Roboto({
     weight: "100",
     subsets: ["latin"],
 })
+
 export default function Page() {
-    return (
-        <html lang="en">
-            <head></head>
-            <main className={roboto.className}>
-                <Navbar />
-                <TypographyH2 />
-            </main>
-        </html>
-    )
+    return <main className={roboto.className}>{/* <TypographyH2 /> */}</main>
 }
--- src/components/ui/themeToggle.jsx
+++ src/components/ui/themeToggle.jsx
@@ -1,27 +1,26 @@
-'use client'
+"use client"
 
-import React from 'react'
+import React, { useEffect, useState } from "react"
 import { Button } from "@/components/ui/button"
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 
 const ThemeToggle = () => {
-  const { theme, setTheme } = useTheme()
-
-  return (
-    <Button
-      variant="ghost"
-      size="icon"
-      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-      className="hover:bg-blue-600"
-    >
-      {theme === "light" ? (
-        <Moon className="h-5 w-5 text-white" />
-      ) : (
-        <Sun className="h-5 w-5 text-white" />
-      )}
-    </Button>
-  )
+    const { theme, setTheme } = useTheme()
+    return (
+        <Button
+            variant="ghost"
+            size="icon"
+            onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+            className="hover:bg-blue-600"
+        >
+            {theme === "light" ? (
+                <Moon className="h-5 w-5 text-white" />
+            ) : (
+                <Sun className="h-5 w-5 text-white" />
+            )}
+        </Button>
+    )
 }
 
 export default ThemeToggle
