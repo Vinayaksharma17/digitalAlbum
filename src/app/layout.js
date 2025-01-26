396e493674c59ebabc0962d70cecbe6053dbf856
abhishekBhure <abhiab7411@gmail.com>
2025-01-26 15:57:05 +0530

    added buttons and image component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,21 @@
+import Imagecomponent from "@/components/ui/Imagecomponent"
 import RootLayout from "./rootLayout"
-import TypographyH2 from "@/components/ui/typography"
+import { Button } from "@/components/ui/button"
+import { LayoutPanelLeft, Trash } from "lucide-react";
 
 export default function Layout({ children }) {
     return (
         <RootLayout>
-            <TypographyH2 />
+            {/* <TypographyH2 /> */}
+            <h1>Hi</h1>
+            <div className="flex items-center justify-center gap-3">
+                <Button variant="outline">Simple</Button>
+                <Button variant="outline" icon={<LayoutPanelLeft />} >Grid</Button>
+            </div>
+            <div className='flex flex-wrap items-center gap-3'>
+                <Imagecomponent src={"https://img.freepik.com/free-vector/26th-january-indian-national-day_23-2148386170.jpg?t=st=1737881174~exp=1737884774~hmac=41c8e6460219104d9aa6644fd2643d0fe290e83bd2473b04ef83ec99ef20b82e&w=900"} alt={"dummy image"} className="" />
+                <Imagecomponent src={"https://img.freepik.com/free-vector/26th-january-indian-national-day_23-2148386170.jpg?t=st=1737881174~exp=1737884774~hmac=41c8e6460219104d9aa6644fd2643d0fe290e83bd2473b04ef83ec99ef20b82e&w=900"} alt={"dummy image"} className="" />
+            </div>
         </RootLayout>
     )
 }
